b9cdb6914579624667268e32dbb553ed13bd6094
Austin Rittenhouse <raustinj39@gmail.com>
2025-06-09 22:49:36 -0500

    🧹 Refactored Firebase imports and cleaned codebase

--- App/config/firebaseApp.ts
+++
@@ -1,4 +0,0 @@
-// App/config/firebaseApp.ts
-// Importing @react-native-firebase/app triggers automatic initialization
-// using the native Google services configuration.
-import '@react-native-firebase/app';
