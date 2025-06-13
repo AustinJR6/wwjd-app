8c0b537c8bd45846d11aa416823ddb94abe36338
Austin Rittenhouse <raustinj39@gmail.com>
2025-06-12 22:09:30 -0500

    🔥 Replaced firebaseConfig.ts with firebase.ts and updated imports across app

--- App/config/firebaseConfig.ts
+++
@@ -1,7 +0,0 @@
-import auth from '@react-native-firebase/auth';
-import firestore from '@react-native-firebase/firestore';
-import storage from '@react-native-firebase/storage';
-
-export const firebaseAuth = auth();
-export const db = firestore();
-export const storageRef = storage();
