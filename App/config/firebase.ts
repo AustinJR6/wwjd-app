52f5fab7a187f6dbb17760917accb1548ce26c9c
Austin Rittenhouse <raustinj39@gmail.com>
2025-06-14 13:05:11 -0500

    Manually initialize Firebase app

--- App/config/firebase.ts
+++ App/config/firebase.ts
@@ -1,3 +1,10 @@
+// Ensure the Firebase native app is initialized
+import { firebase } from '@react-native-firebase/app';
+
+if (!firebase.apps.length) {
+  firebase.initializeApp();
+}
+
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 import storage from '@react-native-firebase/storage';
