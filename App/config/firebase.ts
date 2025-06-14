2646f3193f94c868add8ec12a8064a5f9feb975c
Austin Rittenhouse <raustinj39@gmail.com>
2025-06-14 11:13:17 -0500

    fix: ensure firebase app initialization

--- App/config/firebase.ts
+++ App/config/firebase.ts
@@ -1,3 +1,6 @@
+// Ensure the Firebase native app is initialized
+import '@react-native-firebase/app';
+
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
 import storage from '@react-native-firebase/storage';
