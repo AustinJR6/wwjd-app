9dcb5ef807fc993843572bd9847a429e4eeaa75f
Austin Rittenhouse <raustinj39@gmail.com>
2025-06-15 06:45:17 -0500

    feat: ensure firebase web sdk exports auth

--- App/config/firebase.ts
+++ App/config/firebase.ts
@@ -1,6 +1,7 @@
 // firebase.ts
 
 import { initializeApp, getApps, getApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
@@ -16,7 +17,8 @@ const firebaseConfig = {
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
 
-const firestore = getFirestore(app);
-const storage = getStorage(app);
+export const auth = getAuth(app);
+export const firestore = getFirestore(app);
+export const storage = getStorage(app);
 
-export { app, firestore, storage };
+export { app };
