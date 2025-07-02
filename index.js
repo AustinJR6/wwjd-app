c8d45afa3d54130763d2066d5a9c8808510353f2
Austin Rittenhouse <raustinj39@gmail.com>
2025-07-02 09:25:29 -0500

    chore: clean firebase sdk remnants

--- index.js
+++
@@ -1,6 +0,0 @@
-/* eslint-disable */
-const { AppRegistry } = require('react-native');
-const App = require('./App').default;
-const { name: appName } = require('./app.json');
-
-AppRegistry.registerComponent(appName, () => App);
