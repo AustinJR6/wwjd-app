885a8f05ae86d32a6178f1e6f077a39a5ade2dae
Austin Rittenhouse <raustinj39@gmail.com>
2025-07-03 12:31:04 -0500

    fix: update android config for EAS

--- app.config.js
+++ app.config.js
@@ -4,39 +4,7 @@ export default ({ config }) => ({
   slug: "onevine-app",
   version: "1.0.0",
   runtimeVersion: "1.0.0",
-  orientation: "portrait",
-  icon: "./assets/icon.png",
-  splash: {
-    image: "./assets/splash.png",
-    resizeMode: "contain",
-    backgroundColor: "#ffffff",
-  },
-  userInterfaceStyle: "light",
   assetBundlePatterns: ["**/*"],
-  ios: {
-    supportsTablet: true,
-  },
-  android: {
-    package: "com.lysara.onevine",
-    googleServicesFile:
-      process.env.GOOGLE_SERVICES_JSON || "./android/app/google-services.json",
-  },
-  plugins: [
-    "expo-dev-client",
-    "expo-font",
-    "expo-secure-store",
-    "expo-system-ui",
-    "expo-web-browser",
-    [
-      "expo-build-properties",
-      {
-        android: {
-          kotlinVersion: "1.9.24",
-          gradlePluginVersion: "8.4.1",
-        },
-      },
-    ],
-  ],
   extra: {
     eas: {
       projectId: "bbf209be-1b48-4f76-a496-9d4fcd8339fd",
