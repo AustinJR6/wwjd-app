24f91296730c53d75d478b5d7770b5ac5a8c9f99
Austin Rittenhouse <raustinj39@gmail.com>
2025-07-03 13:37:08 -0500

    stuff

--- app.config.js
+++ app.config.js
@@ -5,9 +5,12 @@ export default ({ config }) => ({
   version: "1.0.0",
   runtimeVersion: "1.0.0",
   assetBundlePatterns: ["**/*"],
+  android: {
+    package: "com.whippybuckle.onevineapp"
+  },
   extra: {
     eas: {
-      projectId: "bbf209be-1b48-4f76-a496-9d4fcd8339fd",
-    },
-  },
+      projectId: "bbf209be-1b48-4f76-a496-9d4fcd8339fd"
+    }
+  }
 });
